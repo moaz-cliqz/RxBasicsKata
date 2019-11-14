59cbe33dae6102e6927ac0b39023e78764d7a8ed
moaz <moaz_carly>
2019-11-14 15:59:59 +0100

    first challenge

--- src/main/java/CountriesServiceSolved.java
+++ src/main/java/CountriesServiceSolved.java
@@ -7,68 +7,70 @@ import io.reactivex.Single;
 
 class CountriesServiceSolved implements CountriesService {
 
-    @Override
-    public Single<String> countryNameInCapitals(Country country) {
-        return null; // put your solution here
-    }
+	@Override
+	public Single<String> countryNameInCapitals(Country country) {
+		return Single.just(country.name.toUpperCase());
+	}
 
-    public Single<Integer> countCountries(List<Country> countries) {
-        return null; // put your solution here
-    }
+	public Single<Integer> countCountries(List<Country> countries) {
+		return Single.just(countries.size());
+	}
 
-    public Observable<Long> listPopulationOfEachCountry(List<Country> countries) {
-        return null; // put your solution here;
-    }
+	public Observable<Long> listPopulationOfEachCountry(List<Country> countries) {
+		return Observable.fromIterable(countries).flatMap(v -> Observable.just(v.population));
+	}
 
-    @Override
-    public Observable<String> listNameOfEachCountry(List<Country> countries) {
-        return null; // put your solution here
-    }
+	@Override
+	public Observable<String> listNameOfEachCountry(List<Country> countries) {
+		return Observable.fromIterable(countries).flatMap(v -> Observable.just(v.name));
+	}
 
-    @Override
-    public Observable<Country> listOnly3rdAnd4thCountry(List<Country> countries) {
-        return null; // put your solution here
-    }
+	@Override
+	public Observable<Country> listOnly3rdAnd4thCountry(List<Country> countries) {
 
-    @Override
-    public Single<Boolean> isAllCountriesPopulationMoreThanOneMillion(List<Country> countries) {
-        return null; // put your solution here
-    }
+		return Observable.merge(Observable.fromArray(countries).elementAt(3,countries),Observable.fromArray(countries).elementAt(4,countries));
 
-    @Override
-    public Observable<Country> listPopulationMoreThanOneMillion(List<Country> countries) {
-        return null; // put your solution here
-    }
+	}
 
-    @Override
-    public Observable<Country> listPopulationMoreThanOneMillionWithTimeoutFallbackToEmpty(final FutureTask<List<Country>> countriesFromNetwork) {
-        return null; // put your solution here
-    }
+	@Override
+	public Single<Boolean> isAllCountriesPopulationMoreThanOneMillion(List<Country> countries) {
+		return null; // put your solution here
+	}
 
-    @Override
-    public Observable<String> getCurrencyUsdIfNotFound(String countryName, List<Country> countries) {
-        return null; // put your solution here
-    }
+	@Override
+	public Observable<Country> listPopulationMoreThanOneMillion(List<Country> countries) {
+		return null; // put your solution here
+	}
 
-    @Override
-    public Observable<Long> sumPopulationOfCountries(List<Country> countries) {
-        return null; // put your solution here
-    }
+	@Override
+	public Observable<Country> listPopulationMoreThanOneMillionWithTimeoutFallbackToEmpty(final FutureTask<List<Country>> countriesFromNetwork) {
+		return null; // put your solution here
+	}
 
-    @Override
-    public Single<Map<String, Long>> mapCountriesToNamePopulation(List<Country> countries) {
-        return null; // put your solution here
-    }
+	@Override
+	public Observable<String> getCurrencyUsdIfNotFound(String countryName, List<Country> countries) {
+		return null; // put your solution here
+	}
 
-    @Override
-    public Observable<Long> sumPopulationOfCountries(Observable<Country> countryObservable1,
-                                                     Observable<Country> countryObservable2) {
-        return null; // put your solution here
-    }
+	@Override
+	public Observable<Long> sumPopulationOfCountries(List<Country> countries) {
+		return null; // put your solution here
+	}
 
-    @Override
-    public Single<Boolean> areEmittingSameSequences(Observable<Country> countryObservable1,
-                                                    Observable<Country> countryObservable2) {
-        return null; // put your solution here
-    }
+	@Override
+	public Single<Map<String, Long>> mapCountriesToNamePopulation(List<Country> countries) {
+		return null; // put your solution here
+	}
+
+	@Override
+	public Observable<Long> sumPopulationOfCountries(Observable<Country> countryObservable1,
+													 Observable<Country> countryObservable2) {
+		return null; // put your solution here
+	}
+
+	@Override
+	public Single<Boolean> areEmittingSameSequences(Observable<Country> countryObservable1,
+													Observable<Country> countryObservable2) {
+		return null; // put your solution here
+	}
 }
